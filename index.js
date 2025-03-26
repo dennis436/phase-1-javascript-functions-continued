28644238787ae62fe1b12d90dc65722229b9ec58
dennis.muchiri1@student.moringaschool.com <dennis.muchiri1@student.moringaschool.com>
2025-03-26 10:25:38 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Function 1: `saturdayFun`
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function 2: `mondayWork`
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Function 3: `wrapAdjective`
+function wrapAdjective(wrapper = "*") {
+  return function (adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
